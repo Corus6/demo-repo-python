f004209f168032ae37fc1a53e39227be84cf6328
Corus6 <148628384+Corus6@users.noreply.github.com>
2025-10-01 19:56:42 +0200

    Add files via upload

---
+++ Tasks_01.py
@@ -0,0 +1,23 @@
+#1
+name = "Filip"
+age = 24
+occupation = "Student"
+print(f"Hello, my name is {name}, I am {age} years old, and I am a {occupation}")
+
+#2
+a = "Hello World"
+print(a[:5])
+
+#3
+b = a.upper()
+print(b)
+
+#4
+c = a.replace("World", "Filip")
+print(c)
+
+#5
+"""
+A comment is ignored by the Python interpreter,
+whereas a print() output is visible.
+"""
